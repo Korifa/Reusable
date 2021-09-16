4473bc8bf49df4ee97bac66aeed9c2b55bbb9650
vitaliy <vitaliykorifa@gmail.com>
2021-09-16 23:38:24 +0300

    add implementation for first 4 functions

--- Exercises/4-range.js
+++ Exercises/4-range.js
@@ -1,5 +1,12 @@
 'use strict';
 
-const range = null;
+const range = (st, end) => {
+  const res = [];
+  if (end - st < 0) return res;
+  for (let i = st; i <= end; i++) {
+    res.push(i);
+  }
+  return res;
+};
 
 module.exports = { range };
